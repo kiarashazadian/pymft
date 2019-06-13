b79bddac6d8192911b28b249dcfb187f19146df1
kiarash <kiaazad123@gmail.com>
2019-06-13 19:58:16 +0430

    testing

---
+++ s01/myfirst.py
@@ -0,0 +1,7 @@
+# bmi=weight/ hight*hight
+weight = input("what is your weight?")
+weight_number = float(weight)
+hight = input("what is your hight?")
+hight_number = float(hight)
+bmi = weight_number / (hight_number * hight_number)
+print(bmi)
