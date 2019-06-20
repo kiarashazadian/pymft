90dab936a62af3a645332a54973be20b9e1642e9
kiarash <kiaazad123@gmail.com>
2019-06-20 17:00:27 +0430

    testing

--- s02/tamrin.py
+++ s02/tamrin.py
@@ -3,3 +3,14 @@ a = sum(my_list)
 b = a / len(my_list)
 print(len(my_list))
 print(b)
+list_1 = [1, 2, 3]
+list_2 = [1, 2, 4]
+list_3 = list_1[2] + list_2[1]
+print(list_3)
+print(list_1)
+new = (1, 2, 3, 4)
+print(new)
+print(*new)
+my_list_2 = [1, 2, 3, 4, 5, 6, 7]
+print(my_list_2[::-2])
+print(my_list_2[:5:1])
